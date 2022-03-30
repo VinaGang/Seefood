933fc825d4226a92d90070ed61cc1a102831fc41
John Manacup <john.manacup@gmail.com>
2022-03-29 22:40:18 -0500

    added test activity

--- app/src/main/java/com/example/seefood/MainActivity.java
+++ app/src/main/java/com/example/seefood/MainActivity.java
@@ -1,7 +1,10 @@
 package com.example.seefood;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,13 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button sampleBtn = findViewById(R.id.sampleBtn);
+
+        sampleBtn.setOnClickListener(view -> {
+
+            Intent i = new Intent(this, TestActivity.class);
+            startActivity(i);
+        });
     }
 }
