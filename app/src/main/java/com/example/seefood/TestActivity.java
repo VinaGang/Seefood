5ca7fcb47786b435126cbf0304d16010c545d066
John Manacup <john.manacup@gmail.com>
2022-03-29 23:19:44 -0500

    Added firebase auth

--- app/src/main/java/com/example/seefood/TestActivity.java
+++ app/src/main/java/com/example/seefood/TestActivity.java
@@ -1,8 +1,7 @@
 package com.example.seefood;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-
+import androidx.appcompat.app.AppCompatActivity;
 public class TestActivity extends AppCompatActivity {
 
     @Override
